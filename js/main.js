39fbbfe0a146c58a25911292a88b754ccd693749
FabrizioCmp <fabriziocampari01@gmail.com>
2022-10-27 14:13:39 +0200

    code formatting

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const distance = prompt("inserisci il numero di kilometri che vuoi percorrere");
 const age = prompt("inserisci la tua età");
-let price = distance * 0.21 ;
+let price = distance * 0.21;
 
 if (age < 18) {
     price = price * 0.8;
-}else if (age > 65) {
+} else if (age > 65) {
     price = price * 0.6;
 }
 
-price = price.toFixed(2)
+price = price.toFixed(2);
 
 const h1Ticket = document.getElementById("ticket");
 h1Ticket.innerHTML = `${price} &euro;`;
